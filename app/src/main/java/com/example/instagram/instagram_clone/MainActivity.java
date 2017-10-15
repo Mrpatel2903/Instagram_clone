3238c05ba6ce7db5a35f118a3d1b134a291cdf30
Mrpatel2903 <mjpatel210821@gmail.com>
2017-10-15 09:58:29 +0530

    Login, registration and home page is created

--- app/src/main/java/com/example/instagram/instagram_clone/MainActivity.java
+++ app/src/main/java/com/example/instagram/instagram_clone/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.instagram.instagram_clone;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.example.instagram.instagram_clone.instagramposthome.InstagramHome;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText userEmailEditText, userPasswordEditText;
     LinearLayout loginLayoutBtn, createAccountLayoutBtn;
+
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,36 +46,131 @@ public class MainActivity extends AppCompatActivity {
         loginLayoutBtn = (LinearLayout) findViewById(R.id.loginButtonMain);
         createAccountLayoutBtn = (LinearLayout) findViewById(R.id.createAccountButtonMain);
 
+        mProgressDialog = new ProgressDialog(this);
+
+        //FIREBASE AUTHENTICATION INSTANCES
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                //CHECKING USER PRESENCE
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if( user != null )
+                {
+
+                    Intent moveToHome = new Intent(MainActivity.this, InstagramHome.class);
+                    moveToHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(moveToHome);
+
+                }
+
+            }
+        };
+
+        mAuth.addAuthStateListener(mAuthListener);
+
         createAccountLayoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 startActivity(new Intent(MainActivity.this, RegisterUserActivity.class));
 
             }
+
         });
 
+        loginLayoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mProgressDialog.setTitle("Loging in the user");
+                mProgressDialog.setMessage("Please wait....");
+                mProgressDialog.show();
+                loginUser();
+
+            }
+        });
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onStart() {
+        super.onStart();
+
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onStop() {
+        super.onStop();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    private void loginUser() {
+
+        String userEmail, userPassword;
+
+        userEmail = userEmailEditText.getText().toString().trim();
+        userPassword = userPasswordEditText.getText().toString().trim();
+
+        if( !TextUtils.isEmpty(userEmail) && !TextUtils.isEmpty(userPassword))
+        {
+
+            mAuth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>()            {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if( task.isSuccessful())
+                    {
+
+                        mProgressDialog.dismiss();
+                        Intent moveToHome = new Intent(MainActivity.this, InstagramHome.class);
+                        moveToHome.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(moveToHome);
+
+                    }else
+                    {
+
+                        Toast.makeText(MainActivity.this, "Unable to login user", Toast.LENGTH_LONG).show();
+                        mProgressDialog.dismiss();
+
+                    }
+
+                }
+            });
+
+        }else
+        {
+
+            Toast.makeText(MainActivity.this, "Please enter user email and password", Toast.LENGTH_LONG).show();
+            mProgressDialog.dismiss();
+
+        }
+
+    }
+
+
+    @Override
+        public boolean onCreateOptionsMenu (Menu menu){
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public boolean onOptionsItemSelected (MenuItem item){
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
     }
-}
