bb8f859fc452d15a5b45a934113c0695cbf3ee1d
Mrpatel2903 <mjpatel210821@gmail.com>
2017-10-19 12:33:16 +0530

    Instagram post created

---
+++ app/src/main/java/com/example/instagram/instagram_clone/instagramposthome/FirebaseRecyclerAdapter.java
@@ -0,0 +1,8 @@
+package com.example.instagram.instagram_clone.instagramposthome;
+
+/**
+ * Created by Yash on 19-10-2017.
+ */
+
+class FirebaseRecyclerAdapter {
+}
--- app/src/main/java/com/example/instagram/instagram_clone/instagramposthome/InstagramPost.java
+++ app/src/main/java/com/example/instagram/instagram_clone/instagramposthome/InstagramPost.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.instagram.instagram_clone.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class InstagramPost extends Fragment {
@@ -22,6 +26,10 @@ public class InstagramPost extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;*/
+    DatabaseReference mInstaPostDatabase, mDatabaseLikes;
+    FirebaseDatabase mAuth;
+    private boolean mlikeProcess = false;
+    RecyclerView firebaseListRecyclerview;
 
     public InstagramPost() {
         // Required empty public constructor
@@ -30,65 +38,31 @@ public class InstagramPost extends Fragment {
     // TODO: Rename and change types and number of parameters
     public static InstagramPost newInstance() {
         InstagramPost fragment = new InstagramPost();
-        /*Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);*/
         return fragment;
     }
-
-    /*@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instagram_post, container, false);
-    }
+        View rootView =  inflater.inflate(R.layout.fragment_instagram_post, container, false);
 
-    /*// TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+        mInstaPostDatabase = FirebaseDatabase.getInstance().getReference().child("Instagram post");
+        mAuth= FirebaseDatabase.getInstance();
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+        mDatabaseLikes = FirebaseDatabase.getInstance().getReference().child("Likes");
+        firebaseListRecyclerview = (RecyclerView) rootView.findViewById(R.id.recyclerInstaPost);
+        firebaseListRecyclerview.setHasFixedSize(true);
+        firebaseListRecyclerview.setLayoutManager(new LinearLayoutManager(getContext()));
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+       /* FirebaseRecyclerAdapter firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Post,PostViewHolder>{
+            Post.class,
+            R.layout.post_card,
+            PostViewHolder.class,
+            mInstaPostDatabase
     }*/
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-   /* public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
+        return rootView;
+    }
+
 }
